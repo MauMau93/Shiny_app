927ea6a2dbb2a179427a37ec96c10d0f72b50559
MauMau93 <mauriciomarcosf@gmail.com>
2021-03-07 21:02:11 +0100

    looks nicer

---
+++ Simple Coding.R
@@ -0,0 +1,15 @@
+# This is a file used to study the data
+
+ColClasses=c(rep("numeric",9))
+DATOS=read.csv2("diabetes.csv",sep = ",",header = T, colClasses = ColClasses, dec = "." )
+summary(DATOS)
+
+#Taking out NAs
+data <- DATOS[2:8]
+data[data==0] <-  NA
+DATOS[2:8] <- data
+DATOS <- mice(DATOS,m=1,method="pmm")
+DATOS <- complete(DATOS)
+attach(DATOS)
+DATOS$Outcome <- as.factor(DATOS$Outcome)
+

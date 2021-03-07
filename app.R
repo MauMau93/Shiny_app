03f10de2bf156ff72fb9b604d6a896b30bd7a6b8
MauMau93 <mauriciomarcosf@gmail.com>
2021-03-07 20:53:33 +0100

    first panel - not finished

---
+++ app.R
@@ -0,0 +1,101 @@
+library(shiny)
+library(RColorBrewer)
+
+
+ColClasses=c(rep("numeric",9))
+DATOS=read.csv2("diabetes.csv",sep = ",",header = T, colClasses = ColClasses, dec = "." )
+feature.list <- colnames(DATOS)
+
+# Define UI for application
+
+
+ui <- navbarPage("Diabetes app",
+                 tabPanel("Feature Inspection",
+                          fluidRow(                           
+                              column(4, selectInput("featureDisplay_x", 
+                                                    label = h3("X-Axis Feature"), 
+                                                    choices = feature.list,
+                                                    selected = feature.list[1])),
+                              column(4, selectInput("featureDisplay_y", 
+                                                    label = h3("Y-Axis Feature"), 
+                                                    choices = feature.list,
+                                                    selected = feature.list[2]))
+                              
+                          ),
+                          fluidRow(
+                              column(4,
+                                     plotOutput("distPlotA")
+                              ),                              
+                              column(4,
+                                     plotOutput("distPlotB")      
+                              ),
+                              column(4,
+                                     plotOutput("ScatterPlot")
+                              )
+                          )
+                          
+                          
+                 ),
+                 
+                 
+                 tabPanel("Feature Descriptions",
+                          fluidRow(
+                              column(10,
+                                     includeMarkdown("Untitled.Rmd")
+                              )
+                          )
+                          
+                 ),
+                 
+                 tabPanel("References",
+                          fluidRow(
+                              column(10,
+                                     includeMarkdown("references.Rmd")
+                              )
+                          )
+                          
+                 )
+)
+
+server <- function(input, output, session) {
+    observe({
+        input_feature_x <- as.symbol(input$featureDisplay_x)
+        input_feature_y <- as.symbol(input$featureDisplay_y)
+        
+        output$distPlotA <- renderPlot({
+            ggplot(DATOS, aes_string(input$featureDisplay_x, fill = "Outcome")) + 
+                geom_histogram(position = "dodge") + 
+                labs(x = input$featureDisplay_x,
+                     y = "Count") + fte_theme() +
+                scale_fill_manual(guide = F,values=c("#7A99AC", "#E4002B")) 
+            
+        })
+        
+        output$distPlotB <- renderPlot({
+            ggplot(DATOS, aes_string(input$featureDisplay_y, 
+                                  fill = "Outcome")) + 
+                geom_histogram(position = "dodge") +
+                labs(x = input$featureDisplay_y,
+                     y = "Count") + fte_theme() +
+                scale_fill_manual(guide = F,values=c("#7A99AC", "#E4002B")) 
+            
+            
+        })
+        
+        output$ScatterPlot <- renderPlot({
+            ggplot(DATOS, aes_string(x = input$featureDisplay_x, 
+                                  y = input$featureDisplay_y, 
+                                  color = "Outcome")) + 
+                geom_point(size = 4, position = position_jitter(w = 0.1, h = 0.1)) + 
+                labs(x = input$featureDisplay_x,
+                     y = input$featureDisplay_y) +
+                fte_theme() + 
+                scale_color_manual(name = "Diabetes",values=c("#7A99AC", "#E4002B")) 
+        })          
+        
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
